45ad641b5411eb39e5a0561ff2068e3f2b5cdbd5
Rohan Singla <rohansinglawork@gmail.com>
2026-06-08 15:18:47 +0530

    code example

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,97 @@
-use axum::{routing::get, Router};
-use socketioxide::{SocketIo, extract::SocketRef};
-use tokio::net::TcpListener;
-use tower::ServiceBuilder;
-use tower_http::cors::CorsLayer;
-use tracing::info;
-use tracing_subscriber::FmtSubscriber;
+use axum::{
+    extract::{
+        ws::{Message, WebSocket, WebSocketUpgrade},
+        State,
+    },
+    response::Response,
+    routing::get,
+    Router,
+};
+use futures_util::{SinkExt, StreamExt};
+use std::sync::Arc;
+use tokio::sync::broadcast;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tracing::subscriber::set_global_default(FmtSubscriber::new())?;
-
-    let (layer,io) = SocketIo::new_layer();
-
-    io.ns("/", on_connect);
-
-    
-
-    let app = Router::new().route("/", get(|| async { "Hello world" }))
-    
-    .layer(
-        ServiceBuilder::new()
-        .layer(CorsLayer::permissive()).
-        layer(layer),
-    
-    );
-
-    info!("starting server...");
-
-    let listener = TcpListener::bind("0.0.0.0:3000").await?;
-
-    axum::serve(listener, app).await?;
-
-    Ok(())
+#[derive(Clone)]
+struct AppState {
+    tx: broadcast::Sender<String>,
 }
 
-async fn on_connect (socket : SocketRef){
+#[tokio::main]
+async fn main() {
+    let (tx, _) = broadcast::channel::<String>(100);
 
-    info!("socket connected ! {:#?}" , socket.id);
+    let state = Arc::new(AppState { tx });
 
+    let app = Router::new()
+        .route("/ws", get(ws_handler))
+        .with_state(state);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .unwrap();
+
+    println!("Server running on 0.0.0.0:3000");
+
+    axum::serve(listener, app)
+        .await
+        .unwrap();
+}
+
+async fn ws_handler(
+    ws: WebSocketUpgrade,
+    State(state): State<Arc<AppState>>,
+) -> Response {
+    ws.on_upgrade(move |socket| handle_socket(socket, state))
+}
+
+async fn handle_socket(
+    socket: WebSocket,
+    state: Arc<AppState>,
+) {
+    println!("Client connected");
+
+    let (mut sender, mut receiver) = socket.split();
+
+    let tx = state.tx.clone();
+    let mut rx = tx.subscribe();
+
+    // Task: receive messages from client
+    let receive_task = tokio::spawn(async move {
+        while let Some(Ok(msg)) = receiver.next().await {
+            match msg {
+                Message::Text(text) => {
+                    println!("Received: {}", text);
+
+                    let _ = tx.send(text.to_string());
+                }
+
+                Message::Close(_) => {
+                    println!("Client disconnected");
+                    break;
+                }
+
+                _ => {}
+            }
+        }
+    });
+
+    // Task: send broadcast messages to client
+    let send_task = tokio::spawn(async move {
+        while let Ok(msg) = rx.recv().await {
+            if sender
+                .send(Message::Text(msg.into()))
+                .await
+                .is_err()
+            {
+                break;
+            }
+        }
+    });
+
+    tokio::select! {
+        _ = receive_task => {}
+        _ = send_task => {}
+    }
+
+    println!("Connection closed");
 }
